93891188834c2868e00d8b93960233e6120eb4eb
ohalikov <olegway31@gmail.com>
2022-10-13 18:25:28 +0500

    work with objects

---
+++ course/es6/objects/script.js
@@ -0,0 +1,30 @@
+const createCar = (name, model) => ({name, model})
+
+
+const ford = createCar('Ford', 'Focus')
+
+content = document.querySelector('.under-header')
+// console.log(ford)
+content.innerHTML = `Машина ${ford.name} модель ${ford.model}`
+
+
+
+const yearField = 'year'
+const bmw = {
+    name: 'BMW',
+    ['model']: 'X6 Sport',
+    [yearField]: 2018,
+
+    logFields() {
+        const {name, model, year} = this
+        console.log(name, model, year)
+
+    }
+}
+
+bmw.logFields()
+
+//const year = bmw.year
+const {year} = bmw
+
+console.log(year)
