5b234f57f58ab5f1dc6fe51f25adf5a5e5baa80c
ohalikov <olegway31@gmail.com>
2022-10-18 15:28:06 +0500

    thirt example my own Promises

---
+++ course/async/promises/thirt_example/script.js
@@ -0,0 +1,17 @@
+
+const coffee = new Promise( (resole, reject) => {
+    setTimeout( () => {
+        message = 'Ваш кофе готов!' 
+        resole(message)
+    }, 3000);
+});
+
+coffee
+    .then( data => {
+        console.log(data);
+    })
+    .catch( err => {
+        console.log(err);
+    })
+
+
