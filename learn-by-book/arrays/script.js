4177a31aebb7ba0e67a4b781e4f38799b27f59b1
o.halikov <o.halikov@ocas.ai>
2023-02-15 19:01:56 +0500

    add folder array

---
+++ learn-by-book/arrays/script.js
@@ -0,0 +1,8 @@
+let fruits = ["Яблоки", "Груша", "Апельсин"];
+
+// добавляем новое значение в "копию"
+let shoppingCart = fruits;
+shoppingCart.push("Банан");
+
+console.log(fruits)
+console.log(shoppingCart)
