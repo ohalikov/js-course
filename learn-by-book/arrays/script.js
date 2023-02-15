299fb71955c1cf45a7069633bf5731c4e72ae3e7
o.halikov <o.halikov@ocas.ai>
2023-02-15 19:01:56 +0500

    add folder array

--- learn-by-book/arrays/script.js
+++ learn-by-book/arrays/script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Цикл for of - по значениям
 // Цикл for in - по всем свойствам объекта проходит, а не только по цифрам
 // for in в 10 -100 раз медленее, так как оптимизирован под произвольные объекты
@@ -11,3 +12,13 @@ const divElement = document.querySelector('.first');
 divElement.innerHTML = `fruits[123] -> ${fruits[123]}, <br>length = ${fruits.length}`;
 
 console.log(fruits.length);
+=======
+let fruits = ["Яблоки", "Груша", "Апельсин"];
+
+// добавляем новое значение в "копию"
+let shoppingCart = fruits;
+shoppingCart.push("Банан");
+
+console.log(fruits)
+console.log(shoppingCart)
+>>>>>>> 4177a31 (add folder array)
