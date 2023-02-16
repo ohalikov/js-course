057c379ade0a16a08f786e7dcbefd81647c4e8fb
o.halikov <o.halikov@ocas.ai>
2023-02-16 10:24:42 +0500

    add new task

--- learn-by-book/arrays_2/script.js
+++ learn-by-book/arrays_2/script.js
@@ -10,13 +10,36 @@ let fruits1 = ["Яблоки", "Груша", "Апельсин"];
 let shoppingCart = fruits1;
 shoppingCart.push("Банан");
 
-console.log(fruits)
+console.log(fruits1)
 console.log(shoppingCart)
 
 let fruits2 = [];
-fruits[123] = 'Яблоко';
+fruits2[123] = 'Яблоко';
 
 const divElement = document.querySelector('.first');
 divElement.innerHTML = `fruits[123] -> ${fruits2[123]}, <br>length = ${fruits2.length}`;
 
 console.log(fruits2.length);
+
+
+// task get MaxSubSum
+
+function getMaxSubSum(arr) {
+    let maxSum = 0;
+    let partialSum = 0;
+
+    for (let item of arr) {
+        partialSum += item
+        maxSum = Math.max(partialSum, maxSum);
+        if (partialSum < 0) partialSum = 0;
+    }
+    return maxSum
+}
+
+
+console.log(getMaxSubSum([-1, 2, 3, -9])); // 5
+console.log(getMaxSubSum([-1, 2, 3, -9, 11])); // 11
+console.log(getMaxSubSum([-2, -1, 1, 2])); // 3
+console.log(getMaxSubSum([100, -9, 2, -3, 5])); // 100
+console.log(getMaxSubSum([1, 2, 3])); // 6
+console.log(getMaxSubSum([-1, -2, -3])); // 0
