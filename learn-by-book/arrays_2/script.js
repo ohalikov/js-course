3972e6f02d3feb97bf472d9e36ccf53322126e53
o.halikov <o.halikov@ocas.ai>
2023-02-16 10:18:28 +0500

    fix changes

--- learn-by-book/arrays_2/script.js
+++ learn-by-book/arrays_2/script.js
@@ -1,24 +1,22 @@
-<<<<<<< HEAD
 // Цикл for of - по значениям
 // Цикл for in - по всем свойствам объекта проходит, а не только по цифрам
 // for in в 10 -100 раз медленее, так как оптимизирован под произвольные объекты
 
 // length - это не длина массива. А наибольший цифровой индекс + 1
 // ЕГО МОЖНО ПЕРЕЗАПИСАТЬ!!!
-let fruits = [];
-fruits[123] = 'Яблоко';
-
-const divElement = document.querySelector('.first');
-divElement.innerHTML = `fruits[123] -> ${fruits[123]}, <br>length = ${fruits.length}`;
-
-console.log(fruits.length);
-=======
-let fruits = ["Яблоки", "Груша", "Апельсин"];
+let fruits1 = ["Яблоки", "Груша", "Апельсин"];
 
 // добавляем новое значение в "копию"
-let shoppingCart = fruits;
+let shoppingCart = fruits1;
 shoppingCart.push("Банан");
 
 console.log(fruits)
 console.log(shoppingCart)
->>>>>>> 4177a31 (add folder array)
+
+let fruits2 = [];
+fruits[123] = 'Яблоко';
+
+const divElement = document.querySelector('.first');
+divElement.innerHTML = `fruits[123] -> ${fruits2[123]}, <br>length = ${fruits2.length}`;
+
+console.log(fruits2.length);
